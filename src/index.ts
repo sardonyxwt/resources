e7887f32372acb6189257a62825113bbd40b74c7
Kyrylo Petkov <sardonyx.wt@gmail.com>
2020-07-09 10:58:15 +0300

    -add helpers.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 const IMAGES_COUNT = 10;
 const AVATARS_COUNT = 30;
 const VIDEOS_COUNT = 1;
+const INFINITE_FLAG = 0;
 
 const preparePathsArray = (
     length: number,
@@ -39,3 +40,31 @@ export const buildVideoPaths = (
         (i) => `${baseDir}/public/video/${i + 1}.mp4`,
     );
 };
+
+export function* sourceListGenerator<T = string>(
+    sources: T[],
+    count = INFINITE_FLAG,
+): Generator<T> {
+    const initIndex = Math.floor(Math.random() * sources.length);
+    for (
+        let i = 0, currentSourceIndex = initIndex;
+        i < count || count === INFINITE_FLAG;
+        i++, currentSourceIndex++
+    ) {
+        if (currentSourceIndex >= sources.length) {
+            currentSourceIndex = 0;
+        }
+        yield sources[currentSourceIndex];
+    }
+}
+
+export function generateSource(sources: string[]): string {
+    return sourceListGenerator(sources).next().value as string;
+}
+
+export function generateSourceList(
+    sources: string[],
+    count = INFINITE_FLAG,
+): string[] {
+    return Array.from(sourceListGenerator(sources, count));
+}
